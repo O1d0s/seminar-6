82bfa6cacce2d977e0f45493e6e64640a5cc1a73
Aidos <ajdos0735@gmaul.com>
2022-12-05 21:08:11 +0600

    Сделал 41 задачу

---
+++ zadacha-41/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Сколько чисел хотите ввести: ");
+int a = int.Parse(Console.ReadLine());
+
+int[] arr = GetArray(a);
+Console.WriteLine($"[{String.Join(", ", arr)}]");
+
+
+Console.Write("Числа больше 0: ");
+Console.WriteLine(BiggerZero(arr));
+
+
+int[] GetArray(int size){
+    int[] mas = new int[size];
+    Console.WriteLine("Введите числа: ");
+    for(int i=0;i<size;i++){
+        mas[i] = int.Parse(Console.ReadLine());
+    }
+    return mas;
+}
+
+int BiggerZero(int[] arr){
+    int count = 0;
+    for(int i=0;i<arr.Length;i++){
+        if(arr[i]>0){
+            count+=1;
+        }
+    }
+    return count;
+}
